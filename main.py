a0e028657fa722cff2ea50878246f778e6d9ef52
Travis Brackney <tbrackney@gmail.com>
2019-10-06 20:16:54 -0700

    completed assignment

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, send_file, Response, render_template_string
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+pig_latinizer = "https://hidden-journey-62459.herokuapp.com"
 
 
 def get_fact():
@@ -17,12 +18,23 @@ def get_fact():
     return facts[0].getText()
 
 
+def pig_latinize(phrase):
+    """Takes a phrase, submits it to the piglatinizer, and returns a link"""
+    url = "https://hidden-journey-62459.herokuapp.com/piglatinize/"
+    # phrase = "what is this"
+    input = {"input_text": phrase}
+    resp = requests.post(url, data=input, allow_redirects=False)
+    return resp.headers.get('Location')
+
+
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact().strip()
+    pl_link = pig_latinize(fact)
+    template = "<a href={{ link }}>{{ link }}</a>"
+    return render_template_string(template, link=pl_link)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
